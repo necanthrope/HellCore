ba169d2a8e97e9debd69ea3456c22f426b666bc4
diatomic.ge <diatomic.ge@gmail.com>
2022-04-24 00:34:40 -0400

    Add nix flake support
    Add a basic nix flake, which can be used by anyone with nix installed to
    reproducibly build hellcore without worrying about installing the right
    packages.
    
    Additionally, if you have direnv and nix available, this can
    automatically pull in all the autoconf/automake/bison/etc. dependencies
    into your current shell, making development more straightforward.
    
    This makes testing more reproducible and less reliant on what is
    available on the host platform, as well as opening up the possibility of
    building hellcore docker images or static-build release binaries with
    relatively small modifications.

---
+++ default.nix
@@ -0,0 +1,12 @@
+# A compatibility file to parse the flake for the default build package if the
+# installed version of nix doesn't support flakes.
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).defaultNix
